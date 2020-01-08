018ac4ade2ec498dd7945f54f31fb59c3b266a50
onagurna <olyscka52@gmail.com>
2020-01-08 20:06:42 +0200

    commit

--- my_diary.py
+++ my_diary.py
@@ -4,27 +4,44 @@ import time
 
 script, filename = argv
 
-print "We're going to erase %r." % filename
+print "We're going to edit %r." % filename
 print '''If you don't want that,write "no" .'''
 print "If you do want that, hit Enter."
-choice = raw_input("?")
-if choice == "no":
-	sys.exit()
+print 'If you do want just read your %r write "read".' % filename
+choice = raw_input()
 
-print "Opening the file..."
-target = open(filename, 'a')
+# if choice == "no":
+# 	sys.exit()
+# elif choice == "read":
+# 	diary = open(filename,'r')
+# 	print diary.read()
+# 	diary.close()
+# 	sys.exit()
 
-print "You can write something"
 
-line = raw_input("Write:")
+target = open(filename,'w+')
+reads = target.read()
+#target.write(reads)
+print'reads: '+ reads
+
+print "You can write something in your diary"
+print "Write:"
+line = raw_input()
+d = len(line)
 
 print "I'm going to write these to the file."
-
+#reads = target.read()
+#target.write(reads)
+#print'reads: '+ reads
 target.write(time.asctime())
-target.write(':  ')
+target.write(':')
+target.write("\n")
+target.write("-" * d)
+target.write("\n")
 target.write(line)
+target.write("\n")
+target.write("-" * d)
 target.write("\n")
 
 
-print "And finally, we close it."
 target.close()
