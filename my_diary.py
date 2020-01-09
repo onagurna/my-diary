37f929cee117ce0b389e1f454506b85442a6d539
onagurna <olyscka52@gmail.com>
2020-01-09 14:15:34 +0200

    init commit

--- my_diary.py
+++ my_diary.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import date
 from sys import argv
 import time
 
@@ -10,19 +11,20 @@ print "If you do want that, hit Enter."
 print 'If you do want just read your %r write "read".' % filename
 choice = raw_input()
 
-# if choice == "no":
-# 	sys.exit()
-# elif choice == "read":
-# 	diary = open(filename,'r')
-# 	print diary.read()
-# 	diary.close()
-# 	sys.exit()
+if choice == "no":
+	sys.exit()
+elif choice == "read":
+	diary = open(filename,'r')
+	print diary.read()
+	diary.close()
+	sys.exit()
 
 
-target = open(filename,'w+')
+target = open(filename,'r+')
 reads = target.read()
-#target.write(reads)
-print'reads: '+ reads
+#print'reads: '+ reads
+target.close()
+target = open(filename,'w+')
 
 print "You can write something in your diary"
 print "Write:"
@@ -30,9 +32,14 @@ line = raw_input()
 d = len(line)
 
 print "I'm going to write these to the file."
-#reads = target.read()
-#target.write(reads)
-#print'reads: '+ reads
+#print reads
+#print len(reads)
+if len(reads) == 0:
+	target.write("-"*10)
+	target.write('My Diary')
+	target.write("-"*10)
+	target.write("\n")
+target.write(reads)
 target.write(time.asctime())
 target.write(':')
 target.write("\n")
