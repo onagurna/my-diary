fcc7fb62ab0a2b7cfc3bf8b4864a852c47e057ae
onagurna <olyscka52@gmail.com>
2020-01-10 17:36:06 +0200

    init commit

--- my_diary.py
+++ my_diary.py
@@ -1,54 +1,52 @@
 import sys
 import datetime
 from sys import argv
-import time
 
 script, filename = argv
 
-print "We're going to edit %r." % filename
-print '''If you don't want that,write "no" .'''
-print "If you do want that, hit Enter."
-print 'If you do want just read your %r write "read".' % filename
-choice = raw_input()
 
-if choice == "no":
-	sys.exit()
-elif choice == "read":
-	diary = open(filename,'r')
-	print diary.read()
-	diary.close()
-	sys.exit()
+while True:
+	print " "
+	print "We're going to edit %r." % filename
+	print '''If you don't want that,write "no" .'''
+	print "If you do want that, hit 'yes'."
+	print 'If you do want just read your %r write "read".' % filename
 
+	choice = raw_input()
+	if choice == "no":
+		sys.exit()
+	elif choice == "read":
+		diary = open(filename,'r')
+		print diary.read()
+		diary.close()
+	elif choice == "yes":
+		target = open(filename,'r+')
+		reads = target.read()
+		target.close()
+		target = open(filename,'w+')
+		print "You can write something in your diary"
+		print "Write:"
 
-target = open(filename,'r+')
-reads = target.read()
-#print'reads: '+ reads
-target.close()
-target = open(filename,'w+')
+		line = raw_input()
+		d = len(line)
+		print "I'm going to write these to the file."
 
-print "You can write something in your diary"
-print "Write:"
-line = raw_input()
-d = len(line)
+		if len(reads) == 0:
+			target.write("-"*10)
+			target.write('My Diary')
+			target.write("-"*10)
+			target.write("\n")
 
-print "I'm going to write these to the file."
-#print reads
-#print len(reads)
-if len(reads) == 0:
-	target.write("-"*10)
-	target.write('My Diary')
-	target.write("-"*10)
-	target.write("\n")
-target.write(reads)
-target.write(str(datetime.date.today()))
-target.write(':')
-target.write("\n")
-target.write("-" * d)
-target.write("\n")
-target.write(line)
-target.write("\n")
-target.write("-" * d)
-target.write("\n")
-
-
-target.close()
+		target.write(reads)
+		target.write(str(datetime.date.today()))
+		target.write(':')
+		target.write("\n")
+		target.write("-" * d)
+		target.write("\n")
+		target.write(line)
+		target.write("\n")
+		target.write("-" * d)
+		target.write("\n")
+		
+		
+		target.close()
