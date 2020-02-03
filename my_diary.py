3172eca5d81c3f57409e705f512ad0649b42b3db
onagurna <olyscka52@gmail.com>
2020-02-03 12:15:55 +0200

    commit

--- my_diary.py
+++ my_diary.py
@@ -12,10 +12,30 @@ while True:
 	print '''If you don't want that,write "no" .'''
 	print "If you do want that, hit 'yes'."
 	print 'If you do want just read your %r write "read".' % filename
+	print 'If you do want find note from some day write "find"'
 
 	choice = raw_input()
 	if choice == "no":
 		sys.exit()
+
+
+	elif choice == "find":
+		target = open(filename,'r+')
+		reads = target.read()
+		target.close()
+		print'Now write this date like year(2020)-mounth(01)-day(01)'
+		note = raw_input()
+		result_date = reads.find(note)
+		end = reads.find("-"*17)
+		if result_date != -1 and end != -1:
+			inf = reads[result_date:end]
+			print inf
+		elif result_date != -1 and end == -1:
+			inf = reads[result_date:-1]
+			print inf
+		elif result_date == -1:
+			print "No data"
+
 	elif choice == "read":
 		diary = open(filename,'r')
 		print diary.read()
@@ -27,13 +47,16 @@ while True:
 		target = open(filename,'w+')
 		print "You can write something in your diary"
 		print "Write:"
+	 
+
 
 		line = raw_input()
-		d = len(line)
+		d_len = len(line)
 		print "I'm going to write these to the file."
 
 		if len(reads) == 0:
-			target.write("-"*10+'My Diary'+"-"*10 +"\n")
+			title = ("-"*10+'My Diary'+"-"*10 +"\n")
+			target.write(title)
 		
 		target.write(reads)
 		named_time = time.localtime()
@@ -41,6 +64,8 @@ while True:
 		date = str(datetime.date.today())
 		result = reads.find(date)
 		if result == -1:
+			if len(reads) != 0:
+				target.write("-"*17)
 			target.write(date +':'+"\n")
 		target.write("-"*10+time_string+"-"*10 +"\n"+line+"\n")
 		
